5e24126fc857235f23314524cc4ab3e4aa9a3a3f
Sumit Gouthaman <sumitgt007@gmail.com>
2014-03-02 11:55:40 +0530

    Adds persistence using chrome.storage.sync

--- js/todo.js
+++ js/todo.js
@@ -12,6 +12,11 @@ todo.controller('todoController', ['$scope',
                 "text": $scope.newTodoText
             })
             $scope.newTodoText = "";
+            chrome.storage.sync.set({
+                "todos": $scope.todos
+            }, function () {
+                console.log("Stuff added to plate");
+            });
         }
 
         $scope.onNewTodoKeyDown = function ($event) {
@@ -23,10 +28,29 @@ todo.controller('todoController', ['$scope',
 
         $scope.removeItem = function (index) {
             $scope.todos.splice(index, 1);
+            chrome.storage.sync.set({
+                "todos": $scope.todos
+            }, function () {
+                console.log("Stuff removed from plate");
+            });
         }
 
         $scope.showOptions = function (index) {
             showOptions(index);
+        }
+
+        $scope.loadData = function () {
+            chrome.storage.sync.get(null, function (data) {
+                var todos = [];
+                if (data.todos) {
+                    todos = data.todos;
+                }
+                $scope.$apply(loadTodos(todos));
+            });
+        }
+
+        loadTodos = function (todos) {
+            $scope.todos = todos;
         }
 
 }]);
